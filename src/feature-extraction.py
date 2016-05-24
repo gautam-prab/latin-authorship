c630654844af88444e4640b47d64d3ca37d58d46
gautam-prab <gautam@venkusha.net>
2016-05-23 20:50:09 -0700

    Add new features, create new datasets, create octave program for SVM machine learning

--- src/feature-extraction.py
+++ src/feature-extraction.py
@@ -8,6 +8,7 @@ import math
 import statistics
 import collections
 import sys
+import os
 
 # iterates thru text and removes every thing in []
 # this is useful for prose with line numbers that we don't want to analyze
@@ -69,11 +70,14 @@ def um_am_ratio(words):
     return um/am
 
 #counts conjunctions
-def conjunction_counter(postag):
-    count = 0
+def pos_counter(postag):
+    count = {}
     for k,v in postag:
-        if v is not None and v[0] is 'C':
-            count = count + 1
+        if v is not None:
+            if v[0] in count:
+                count[v[0]] = count[v[0]] + 1
+            else:
+                count[v[0]] = 1
     return count
 
 #turns most common words in Latin into a list
@@ -83,82 +87,183 @@ def listifyWords():
     toReturn = [x.strip('\n') for x in toReturn]
     return toReturn
 
-# main
+#counts instances of the ablative absolute, a Latin grammatical construction
+#note that this likely undercounts ablative absolutes
+def ablative_absolute(postag):
+    count = 0
+    temp = False
+    for k,v in postag:
+        if v is not None:
+            #checks if the word is an adjective/participle/noun and is ablative
+            if (v[0] is 'A' or v[0] is 'T' or v[0] is 'N') and v[7] is 'B':
+                if temp is False:
+                    temp = True
+                else:
+                    temp = False
+                    count = count + 1
+            else:
+                temp = False
+        else:
+            temp = False
+    return count
 
-if (len(sys.argv) < 3):
-    print("input required")
+#avg words per sentence
+def words_per_sentence(sentences):
+    count = 0
+    for s in sentences:
+        count = count + len(s.split())
+    return count / len(sentences)
 
-jv = JVReplacer()
-words = WordTokenizer('latin')
-tagger = POSTag('latin')
-lem = LemmaReplacer('latin')
+#subjunctive use, passive voice use, infinitive use
+def verb_use(postag):
+    sub_count = 0
+    pass_count = 0
+    inf_count = 0
+    verb_count = 0
+    for k,v in postag:
+        if v is not None and v[0] is 'V':
+            verb_count = verb_count + 1
+            if v[4] is "S":
+                sub_count = sub_count + 1
+            if v[5] is "P":
+                pass_count = pass_count + 1
+            if v[4] is "N":
+                inf_count = inf_count + 1
+    return [sub_count / verb_count, pass_count / verb_count, inf_count / verb_count]
 
-f = open(sys.argv[1]+'.txt', 'r') #infile
-txt = f.read()
 
-f_out = open(sys.argv[1]+'.lat', 'w') #outfile
+# extract features from a single file
+def feature_extract(filename, author):
+    print(filename+" by "+author)
+    jv = JVReplacer()
+    words = WordTokenizer('latin')
+    sentences = TokenizeSentence('latin')
+    tagger = POSTag('latin')
+    lem = LemmaReplacer('latin')
 
-print("wrangling text")
-jv.replace(txt) # regularizes use of "i/j" and "u/v" as these are interchangeable in Latin
-uppercase = txt
-txt = txt.lower() # make lower case
+    f = open(filename+'.txt', 'r') #infile
+    txt = f.read()
 
-txt = removeBrackets(txt)
-no_commas = removePunctuation(txt)
+    f_out = open(filename+'.lat', 'w') #outfile
 
-print("finding parts of speech")
-word_tokens = words.tokenize(no_commas)
-lemma_tokens = lem.lemmatize(no_commas)
-parts_of_speech = tagger.tag_ngram_123_backoff(no_commas)
+    print("wrangling text")
+    jv.replace(txt) # regularizes use of "i/j" and "u/v" as these are interchangeable in Latin
+    uppercase = txt
+    txt = txt.lower() # make lower case
 
-print("word lengths")
-word_lengths = []
-for word in word_tokens:
-    word_lengths+=[len(word)]
+    txt = removeBrackets(txt)
+    no_commas = removePunctuation(txt)
 
-avg_word_length = (statistics.mean(word_lengths) - 6.3)
-st_dev = statistics.stdev(word_lengths)
+    print("finding parts of speech")
+    word_tokens = words.tokenize(no_commas)
+    sentence_tokens = sentences.tokenize_sentences(txt)
+    lemma_tokens = lem.lemmatize(no_commas)
+    parts_of_speech = tagger.tag_ngram_123_backoff(no_commas)
 
-print("legomena")
-hapax, dis = legomena(lemma_tokens)
-freq = word_frequencies(lemma_tokens)
-print("named entities")
-ne = ner.tag_ner('latin', input_text=uppercase, output_type=str)
-print("um/am")
-um_am = um_am_ratio(word_tokens)
-print("conjunctions")
-conj = conjunction_counter(parts_of_speech) / (len(word_tokens)/10)
+    print("word lengths")
+    word_lengths = []
+    for word in word_tokens:
+        word_lengths+=[len(word)]
 
-print("number of words: " + str(len(word_tokens)))
-print("average word length: " + str(avg_word_length))
-print(str(avg_word_length), file=f_out)
-print("standard deviation word length: " + str(st_dev))
-print(str(st_dev), file=f_out)
-print("hapax legomena: " + str(len(hapax) / len(freq)))
-print(str(len(hapax) / len(freq)), file=f_out)
-print("dis legomena: " + str(len(dis) / len(freq)))
-print(str(len(dis) / len(freq)), file=f_out)
-print("richness: " + str(len(freq) / len(word_tokens)))
-print(str(len(freq) / len(word_tokens)), file=f_out)
-commonWords = listifyWords()
-for w in commonWords: #prints the most common Latin words as frequencies
-    w = lem.lemmatize(w, return_string = True)
-    if w in freq.keys():
-        print(w + ": " + str(freq[w]/(len(word_tokens)/10)))
-        print(str(freq[w]/(len(word_tokens)/10)), file=f_out)
-    else:
-        print(w + ": 0")
-        print(0, file=f_out)
-named_entities = ne.count("Entity") / (len(word_tokens) / 10)
-print("ner: "+str(named_entities))
-print(str(named_entities), file=f_out)
-nt_count = txt.count("nt") / (len(no_commas) / 10)
-print("nt frequency: "+str(nt_count))
-print(str(nt_count), file=f_out)
-print("um:am ratio: "+str(um_am))
-print(str(um_am), file=f_out)
-print("conjunction count: "+str(conj))
-print(str(conj), file=f_out)
-author_count = txt.count(sys.argv[2].lower()) / (len(word_tokens)/10)
-print("author name: "+str(author_count))
-print((author_count), file=f_out)
+    avg_word_length = (statistics.mean(word_lengths) - 6.3)
+    st_dev = statistics.stdev(word_lengths)
+
+    print("legomena")
+    hapax, dis = legomena(lemma_tokens)
+    freq = word_frequencies(lemma_tokens)
+    print("named entities")
+    ne = ner.tag_ner('latin', input_text=uppercase, output_type=str)
+    print("um/am")
+    um_am = um_am_ratio(word_tokens)
+    print("parts of speech frequencies")
+    pos_freq = pos_counter(parts_of_speech)
+    verb_freq = verb_use(parts_of_speech)
+    print("ablative absolute")
+    abl = ablative_absolute(parts_of_speech)
+    print("sentence analysis")
+    wps = words_per_sentence(sentence_tokens)
+
+    print("number of words: " + str(len(word_tokens)))
+    print("average word length: " + str(avg_word_length))
+    print(str(avg_word_length), file=f_out)
+    print("standard deviation word length: " + str(st_dev))
+    print(str(st_dev), file=f_out)
+    print("hapax legomena: " + str(len(hapax) / len(freq)))
+    print(str(len(hapax) / len(freq)), file=f_out)
+    print("dis legomena: " + str(len(dis) / len(freq)))
+    print(str(len(dis) / len(freq)), file=f_out)
+    print("richness: " + str(len(freq) / len(word_tokens)))
+    print(str(len(freq) / len(word_tokens)), file=f_out)
+    commonWords = listifyWords()
+    for w in commonWords: #prints the most common Latin words as frequencies
+        w = lem.lemmatize(w, return_string = True)
+        if w in freq.keys():
+            print(w + ": " + str(freq[w]/(len(word_tokens))))
+            print(str(freq[w]/(len(word_tokens))), file=f_out)
+        else:
+            print(w + ": 0")
+            print(0, file=f_out)
+    named_entities = ne.count("Entity") / (len(word_tokens))
+    print("ner: "+str(named_entities))
+    print(str(named_entities), file=f_out)
+    nt_count = txt.count("nt") / (len(no_commas))
+    print("nt frequency: "+str(nt_count))
+    print(str(nt_count), file=f_out)
+    print("um:am ratio: "+str(um_am))
+    print(str(um_am), file=f_out)
+    for c in ['N','V','T','A','D','C','R','P']:
+        if c in pos_freq:
+            print(c + " POS: " + str(pos_freq[c]/(len(word_tokens))))
+            print(str(pos_freq[c]/(len(word_tokens))), file=f_out)
+        else:
+            print(c + " POS: 0")
+            print(0, file=f_out)
+    print("ablative absolute: "+str(abl/len(word_tokens)))
+    print(str(abl/len(word_tokens)), file=f_out)
+    print("subjunctive frequency: "+str(verb_freq[0]))
+    print(str(verb_freq[0]), file=f_out)
+    print("passive frequency: "+str(verb_freq[1]))
+    print(str(verb_freq[1]), file=f_out)
+    print("infinitive frequency: "+str(verb_freq[2]))
+    print(str(verb_freq[2]), file=f_out)
+    print("words per sentence: "+str(wps))
+    print(str(wps), file=f_out)
+    author_count = txt.count(author.lower()) / (len(word_tokens))
+    print("author name: "+str(author_count))
+    print((author_count), file=f_out)
+
+# main iterates through all files in a given directory
+path = sys.argv[1]
+if not path.endswith("/"):
+    path = path + '/'
+
+# create a dictionary of authors
+with open('authors.txt') as authorfile:
+    lines = authorfile.readlines()
+lines = [x.strip('\n') for x in lines]
+author_dict = {}
+for line in lines:
+    vals = line.split('\t')
+    author_dict[vals[0]] = [vals[1], vals[2]]
+
+outfile = input("Dataset Filename: ")
+outf = open(outfile, "w")
+
+for filename in os.listdir(sys.argv[1]):
+    if filename.endswith(".txt"):
+        # first, run feature extraction
+        filename = filename[:-4] #remove last 4 characters, aka .txt
+        author = author_dict[filename][0]
+        feature_extract(path+filename, author)
+
+        # then add to a larger dataset file
+        inf = open(path+filename+".lat", "r")
+        count = 1
+        classification = author_dict[filename][1]
+        outline = str(classification)+" "
+        for line in inf.readlines():
+            line = line[:-1]
+            outline = outline + str(count) + ":" + line + " "
+            count = count + 1
+        print(outline, file=outf)
+        inf.close()
---
+++ src/latin_authorship.m
@@ -0,0 +1,115 @@
+## uses LIBSVM to use machine learning to classify authorship of Latin works
+function [retval] = latin_authorship (training_scaled, cross_validation_scaled, test_scaled)
+	printf("creating datasets\n")
+	[train_lab, train_inst] = libsvmread(training_scaled);
+	[cross_lab, cross_inst] = libsvmread(cross_validation_scaled);
+	[test_lab, test_inst] = libsvmread(test_scaled);
+
+	train_lab_mat = [cast(train_lab == 1, "double"), cast(train_lab == 2, "double"), cast(train_lab == 3, "double"), cast(train_lab == 4, "double"), cast(train_lab == 5, "double"), cast(train_lab == 6, "double"), cast(train_lab == 7, "double"), cast(train_lab == 8, "double")];
+	cross_lab_mat = [cast(cross_lab == 1, "double"), cast(cross_lab == 2, "double"), cast(cross_lab == 3, "double"), cast(cross_lab == 4, "double"), cast(cross_lab == 5, "double"), cast(cross_lab == 6, "double"), cast(cross_lab == 7, "double"), cast(cross_lab == 8, "double")];
+	test_lab_mat = [cast(test_lab == 1, "double"), cast(test_lab == 2, "double"), cast(test_lab == 3, "double"), cast(test_lab == 4, "double"), cast(test_lab == 5, "double"), cast(test_lab == 6, "double"), cast(test_lab == 7, "double"), cast(test_lab == 8, "double")];
+
+	printf("creating models\n")
+	model_mat = [svmtrain(train_lab_mat(:,1), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,2), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,3), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,4), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,5), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,6), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,7), train_inst, '-c 2 -g .0078125 -b 1'); svmtrain(train_lab_mat(:,8), train_inst, '-c 2 -g .0078125 -b 1')];
+
+	printf("predicting training data\n")
+	[predict_label_1, acc_1, prob_1] = svmpredict(train_lab_mat(:,1), train_inst, model_mat(1,:), '-b 1');
+	[predict_label_2, acc_2, prob_2] = svmpredict(train_lab_mat(:,2), train_inst, model_mat(2,:), '-b 1');
+	[predict_label_3, acc_3, prob_3] = svmpredict(train_lab_mat(:,3), train_inst, model_mat(3,:), '-b 1');
+	[predict_label_4, acc_4, prob_4] = svmpredict(train_lab_mat(:,4), train_inst, model_mat(4,:), '-b 1');
+	[predict_label_5, acc_5, prob_5] = svmpredict(train_lab_mat(:,5), train_inst, model_mat(5,:), '-b 1');
+	[predict_label_6, acc_6, prob_6] = svmpredict(train_lab_mat(:,6), train_inst, model_mat(6,:), '-b 1');
+	[predict_label_7, acc_7, prob_7] = svmpredict(train_lab_mat(:,7), train_inst, model_mat(7,:), '-b 1');
+	[predict_label_8, acc_8, prob_8] = svmpredict(train_lab_mat(:,8), train_inst, model_mat(8,:), '-b 1');
+
+	printf("predicting cross-validation\n")
+	[predict_label_1, acc_1, prob_1] = svmpredict(cross_lab_mat(:,1), cross_inst, model_mat(1,:), '-b 1');
+	[predict_label_2, acc_2, prob_2] = svmpredict(cross_lab_mat(:,2), cross_inst, model_mat(2,:), '-b 1');
+	[predict_label_3, acc_3, prob_3] = svmpredict(cross_lab_mat(:,3), cross_inst, model_mat(3,:), '-b 1');
+	[predict_label_4, acc_4, prob_4] = svmpredict(cross_lab_mat(:,4), cross_inst, model_mat(4,:), '-b 1');
+	[predict_label_5, acc_5, prob_5] = svmpredict(cross_lab_mat(:,5), cross_inst, model_mat(5,:), '-b 1');
+	[predict_label_6, acc_6, prob_6] = svmpredict(cross_lab_mat(:,6), cross_inst, model_mat(6,:), '-b 1');
+	[predict_label_7, acc_7, prob_7] = svmpredict(cross_lab_mat(:,7), cross_inst, model_mat(7,:), '-b 1');
+	[predict_label_8, acc_8, prob_8] = svmpredict(cross_lab_mat(:,8), cross_inst, model_mat(8,:), '-b 1');
+
+	printf("predicting test set\n")
+	[predict_label_1, acc_1, prob_1] = svmpredict(test_lab_mat(:,1), test_inst, model_mat(1,:), '-b 1');
+	[predict_label_2, acc_2, prob_2] = svmpredict(test_lab_mat(:,2), test_inst, model_mat(2,:), '-b 1');
+	[predict_label_3, acc_3, prob_3] = svmpredict(test_lab_mat(:,3), test_inst, model_mat(3,:), '-b 1');
+	[predict_label_4, acc_4, prob_4] = svmpredict(test_lab_mat(:,4), test_inst, model_mat(4,:), '-b 1');
+	[predict_label_5, acc_5, prob_5] = svmpredict(test_lab_mat(:,5), test_inst, model_mat(5,:), '-b 1');
+	[predict_label_6, acc_6, prob_6] = svmpredict(test_lab_mat(:,6), test_inst, model_mat(6,:), '-b 1');
+	[predict_label_7, acc_7, prob_7] = svmpredict(test_lab_mat(:,7), test_inst, model_mat(7,:), '-b 1');
+	[predict_label_8, acc_8, prob_8] = svmpredict(test_lab_mat(:,8), test_inst, model_mat(8,:), '-b 1');
+
+	printf("test 1\n")
+	printf("%f\t%f\n", prob_1(1,1),prob_1(1,2))
+	printf("%f\t%f\n", prob_2(1,1),prob_2(1,2))
+	printf("%f\t%f\n", prob_3(1,1),prob_3(1,2))
+	printf("%f\t%f\n", prob_4(1,1),prob_4(1,2))
+	printf("%f\t%f\n", prob_5(1,1),prob_5(1,2))
+	printf("%f\t%f\n", prob_6(1,1),prob_6(1,2))
+	printf("%f\t%f\n", prob_7(1,1),prob_7(1,2))
+	printf("%f\t%f\n\n", prob_8(1,1),prob_8(1,2))
+
+	printf("test 2\n")
+	printf("%f\t%f\n", prob_1(2,1),prob_1(2,2))
+	printf("%f\t%f\n", prob_2(2,1),prob_2(2,2))
+	printf("%f\t%f\n", prob_3(2,1),prob_3(2,2))
+	printf("%f\t%f\n", prob_4(2,1),prob_4(2,2))
+	printf("%f\t%f\n", prob_5(2,1),prob_5(2,2))
+	printf("%f\t%f\n", prob_6(2,1),prob_6(2,2))
+	printf("%f\t%f\n", prob_7(2,1),prob_7(2,2))
+	printf("%f\t%f\n\n", prob_8(2,1),prob_8(2,2))
+
+	printf("test 3\n")
+	printf("%f\t%f\n", prob_1(3,1),prob_1(3,2))
+	printf("%f\t%f\n", prob_2(3,1),prob_2(3,2))
+	printf("%f\t%f\n", prob_3(3,1),prob_3(3,2))
+	printf("%f\t%f\n", prob_4(3,1),prob_4(3,2))
+	printf("%f\t%f\n", prob_5(3,1),prob_5(3,2))
+	printf("%f\t%f\n", prob_6(3,1),prob_6(3,2))
+	printf("%f\t%f\n", prob_7(3,1),prob_7(3,2))
+	printf("%f\t%f\n\n", prob_8(3,1),prob_8(3,2))
+
+	printf("test 4\n")
+	printf("%f\t%f\n", prob_1(4,1),prob_1(4,2))
+	printf("%f\t%f\n", prob_2(4,1),prob_2(4,2))
+	printf("%f\t%f\n", prob_3(4,1),prob_3(4,2))
+	printf("%f\t%f\n", prob_4(4,1),prob_4(4,2))
+	printf("%f\t%f\n", prob_5(4,1),prob_5(4,2))
+	printf("%f\t%f\n", prob_6(4,1),prob_6(4,2))
+	printf("%f\t%f\n", prob_7(4,1),prob_7(4,2))
+	printf("%f\t%f\n\n", prob_8(4,1),prob_8(4,2))
+
+	printf("test 5\n")
+	printf("%f\t%f\n", prob_1(5,1),prob_1(5,2))
+	printf("%f\t%f\n", prob_2(5,1),prob_2(5,2))
+	printf("%f\t%f\n", prob_3(5,1),prob_3(5,2))
+	printf("%f\t%f\n", prob_4(5,1),prob_4(5,2))
+	printf("%f\t%f\n", prob_5(5,1),prob_5(5,2))
+	printf("%f\t%f\n", prob_6(5,1),prob_6(5,2))
+	printf("%f\t%f\n", prob_7(5,1),prob_7(5,2))
+	printf("%f\t%f\n\n", prob_8(5,1),prob_8(5,2))
+
+	printf("test 6\n")
+	printf("%f\t%f\n", prob_1(6,1),prob_1(6,2))
+	printf("%f\t%f\n", prob_2(6,1),prob_2(6,2))
+	printf("%f\t%f\n", prob_3(6,1),prob_3(6,2))
+	printf("%f\t%f\n", prob_4(6,1),prob_4(6,2))
+	printf("%f\t%f\n", prob_5(6,1),prob_5(6,2))
+	printf("%f\t%f\n", prob_6(6,1),prob_6(6,2))
+	printf("%f\t%f\n", prob_7(6,1),prob_7(6,2))
+	printf("%f\t%f\n\n", prob_8(6,1),prob_8(6,2))
+
+	printf("test 7\n")
+	printf("%f\t%f\n", prob_1(7,1),prob_1(7,2))
+	printf("%f\t%f\n", prob_2(7,1),prob_2(7,2))
+	printf("%f\t%f\n", prob_3(7,1),prob_3(7,2))
+	printf("%f\t%f\n", prob_4(7,1),prob_4(7,2))
+	printf("%f\t%f\n", prob_5(7,1),prob_5(7,2))
+	printf("%f\t%f\n", prob_6(7,1),prob_6(7,2))
+	printf("%f\t%f\n", prob_7(7,1),prob_7(7,2))
+	printf("%f\t%f\n\n", prob_8(7,1),prob_8(7,2))
+
+endfunction
